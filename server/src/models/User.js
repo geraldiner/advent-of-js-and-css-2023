86b278719741d4bb5ba89d21e1bdbd091e6bf85d
Geraldine R <hello@geraldiner.com>
2024-01-05 19:29:50 -0500

    feat: Set up backend with express and frontend with react

---
+++ server/src/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const requiredString = {
+  type: String,
+  required: true,
+};
+
+const UserSchema = new Schema(
+  {
+    email: requiredString,
+    password: requiredString,
+    name: requiredString,
+    avatar: String,
+    role: {
+      type: String,
+      enum: ["ADMIN", "USER"],
+      default: "USER",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
---
+++ server/src/models/WishList.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const requiredString = {
+  type: String,
+  required: true,
+};
+
+const requiredNumber = {
+  type: Number,
+  required: true,
+};
+
+const WishListSchema = new Schema(
+  {
+    name: requiredString,
+    url: requiredString,
+    userId: requiredNumber,
+    order: requiredNumber,
+    eventId: requiredString,
+    siteImage: String,
+    siteTitle: String,
+    siteDescription: String,
+  },
+  { timestamps: true }
+);
+
+const WishList = mongoose.model("WishList", WishListSchema);
+
+module.exports = WishList;
