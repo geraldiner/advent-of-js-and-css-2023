a57d1d85e596c498d48e827071465c916508f89c
Quincy Bulla <37887069+dominionworkz@users.noreply.github.com>
2024-01-09 16:59:18 -0500

    LoginPage icon component added

--- client/src/routes/LoginPage.jsx
+++ client/src/routes/LoginPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Footer from '../components/Footer';
+import Icon from '../components/Icon';
 
 function LoginPage() {
   return (
